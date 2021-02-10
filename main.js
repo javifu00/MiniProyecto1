68cddeb3cb049029b2e7bc33ca7b64eb307ece18
Javier Fuenmayor <javierfuenmayor00@gmail.com>
2021-02-09 20:02:53 -0400

    Form con validaciones

--- main.js
+++ main.js
@@ -3,21 +3,28 @@ function mostrarDatos(){
     var nombre = document.getElementById('name');
     var correo = document.getElementById('mail');
     var mensaje = document.getElementById('msg');
-    if (!correo.value.contains("@") && !correo.value.contains(".com")){
+    var verificarCorreo = correo.value.includes("@")
+    if (!validateEmail(correo.value)){
         datosVerificados = false;
-    } else if (hasNumber(nombre.value)){
+    } else if (!allLetter(nombre.value)){
+        datosVerificados = false;
+    } else if (mensaje.value === ""){
         datosVerificados = false;
     }
-
     if (datosVerificados) {
         console.log("Nombre: " + nombre.value + "\nCorreo electronico: " + correo.value + "\nMensaje: " + mensaje.value);
         alert("Su mensaje ha sido enviado");
     } else {
-        alert("Sus datos no son validos");
+        alert("Sus datos no son validos, Ingreselos nuevamente");
     }
     
 }
 
-function hasNumber(myString) {
-    return /\d/.test(myString);
+function validateEmail(mail) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(mail).toLowerCase());
+}
+
+function allLetter(inputtxt){
+    return /^[A-z ]+$/.test(inputtxt);
 }
